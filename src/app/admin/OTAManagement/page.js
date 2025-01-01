9437f5f80c941d0699d48394d4763d0411b22ea6
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2025-01-01 18:32:16 +0500

    Added Responsive LAyout for OA

--- src/app/admin/OTAManagement/page.js
+++ src/app/admin/OTAManagement/page.js
@@ -1,5 +1,4 @@
 'use client'
-import { Option, Select } from '@material-tailwind/react';
 
 import axios from 'axios';
 import Image from 'next/image';
@@ -12,12 +11,50 @@ function Page() {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
     const options = ["2.05.10", "2.05.11", "3.0.0"];
-  
+    const [selectedArcades, setSelectedArcades] = useState([]);
+
+
     const handleSelect = (option) => {
       setSelectedOption(option);
       setIsOpen(false);
     };
+    const [fileName, setFileName] = useState('');
+    const [isDragging, setIsDragging] = useState(false);
   
+    const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        setFileName(file.name);
+      }
+    };
+  
+    const handleDragOver = (event) => {
+      event.preventDefault();
+      setIsDragging(true);
+    };
+  
+    const handleDragLeave = () => {
+      setIsDragging(false);
+    };
+  
+    const handleDrop = (event) => {
+      event.preventDefault();
+      setIsDragging(false);
+      const file = event.dataTransfer.files[0];
+      if (file) {
+        setFileName(file.name);
+      }
+    };
+
+    const handleCheckboxChange = (arcadeId) => {
+      setSelectedArcades((prevSelected) =>
+        prevSelected.includes(arcadeId)
+          ? prevSelected.filter((id) => id !== arcadeId) // Remove if already selected
+          : [...prevSelected, arcadeId] // Add if not selected
+      );
+    };
+
+
 
     useEffect(() => {
         const fetchArcadeList = async () => {
@@ -36,10 +73,78 @@ function Page() {
 
 
     return (
-        <div className='flex items-center sm:flex-row flex-col h-[10] justify-center md:justify-around p-2'>
+        <div className='flex items-center lg:flex-row flex-col h-[10] justify-center md:justify-around p-2'>
             {/* Add OTA */}
             <div>
+                <div className='bg-white p-5 xs:min-w-[60vw] sm:min-w-[500px] rounded-xl'>
 
+                  <h1 className='text-md font-semibold  text-custom-blue'>Add OTA:</h1>
+                  <div className='flex items-center justify-between gap-2'>
+                  <label htmlFor="arcade1" className="text-left font-medium text-gray-700">
+                  OTA Version ID
+                </label>
+                <div className='relative'>
+                <input
+                  type="text"
+                  id="arcade1"
+                  name="arcade1"
+                  className="sm:p-2  p-1 border border-black focus:outline-none rounded-full w-[170px] sm:w-[250px] md:w-[300px]"
+                  placeholder="Enter Text"
+                  />
+                <p className='absolute top-[-10] right-3 '>Eg. 3-05-12</p>
+                </div>
+                  </div>
+
+                  <div
+      className={`bg-[#70CECA] border p-5 border-[#49908D] mt-10 rounded-xl flex items-center justify-center flex-col gap-y-2 ${
+        isDragging ? 'border-dashed border-white' : ''
+      }`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      <Image src="/Cloud.svg" height={100} width={70} alt="Upload Icon" />
+      <h1 className="text-sm text-white">Drag And Drop To Upload File</h1>
+      <h2 className="text-sm text-white">OR</h2>
+      <div className="relative">
+        <label
+          htmlFor="fileUpload"
+          className="bg-white text-[#70CECA] py-1 px-2 rounded-lg w-[200px] sm:w-auto cursor-pointer text-center block"
+        >
+          {fileName ? 'Change File' : 'Browse Files'}
+        </label>
+        <input
+          id="fileUpload"
+          type="file"
+          className="hidden"
+          name="Browse File"
+          onChange={handleFileChange}
+        />
+        {fileName && (
+          <p className="mt-2 text-sm text-gray-500">
+            Selected File: <span className="font-medium">{fileName}</span>
+          </p>
+        )}
+      </div>
+    </div>
+
+                <div className="flex justify-end w-full space-x-4 mt-5 mb-2">
+                <button
+                  type="submit"
+                  className="bg-custom-headblue text-white py-1 px-6 rounded-xl hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="text-red-500 py-1 px-6 rounded-xl border border-black hover:border-red-600 hover:text-black"
+                >
+                  Cancel
+                </button>
+              </div>
+           
+
+                </div>
             </div>
             {/* ARCADE LIST */}
             <div>
@@ -105,58 +210,59 @@ function Page() {
         )}
       </div>
     </div>
-                <div className='rounded-xl bg-white p-5'>
-                    <div className="max-h-[62vh] bg-white min-h-[45vh] md:min-w-[30vw] min-w-[75vw] max-w-[40vw] overflow-y-auto  rounded-xl ">
-                        <h2 className="text-lg text-center pt-5 text-custom-headblue font-bold border-2 border-b-custom-headblue">
-                            Arcade List
-                        </h2>
-                        <ul className="">
-                            {arcadeList.map((arcade) => (
-                                <li
-                                    key={arcade.arcade_id}
-                                    className="flex justify-between items-center bg-white border border-b-black p-3 shadow hover:bg-gray-100"
-                                >
-                                    <div className='break-words sm:whitespace-nowrap xs:text-auto text-sm xs:w-auto w-[130px] px-1'>{arcade.arcade_id}</div>
-                                    <div className="flex space-x-2">
-                                        <button
-                                            className="text-blue-500 hover:text-blue-700"
-                                            onClick={() => handleDeleteArcade(arcade.arcade_id)}
-                                        >
-                                            <Image src="/Delete.svg" height={32} width={32} alt="Delete Icon" />
-                                        </button>
-                                        <button
-                                            className="text-red-500 hover:text-red-700"
-                                            onClick={() => handleEditArcade(arcade)}
-                                        >
-                                            <Image src="/Setting.svg" height={32} width={32} alt="Edit Icon" />
-                                        </button>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-                <div className='bg-white grid grid-cols-2 gap-2'>
+               <div className="rounded-xl bg-white p-5">
+      <div className="max-h-[53vh] bg-white min-h-[45vh] md:min-w-[60vw] xs:min-w-[60vw] lg:min-w-[30vw] min-w-[75vw] max-w-[40vw] overflow-y-auto rounded-xl">
+        <h2 className="text-lg text-center pt-5 text-custom-headblue font-bold border-2 border-b-custom-headblue">
+          Arcade List
+        </h2>
+        <ul>
+          {arcadeList.map((arcade) => (
+            <li
+              key={arcade.arcade_id}
+              className="flex justify-between items-center bg-white border border-b-black p-3 shadow hover:bg-gray-100"
+            >
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  id={`arcade-${arcade.arcade_id}`}
+                  className="form-checkbox h-4 w-4 text-blue-600"
+                  checked={selectedArcades.includes(arcade.arcade_id)}
+                  onChange={() => handleCheckboxChange(arcade.arcade_id)}
+                />
+                <label
+                  htmlFor={`arcade-${arcade.arcade_id}`}
+                  className="break-words sm:whitespace-nowrap xs:text-auto text-sm xs:w-auto w-[130px] px-1 cursor-pointer"
+                >
+                  {arcade.arcade_id}
+                </label>
+              </div>
+              <div className="flex space-x-2">2.1.23</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+                <div className='bg-white grid grid-cols-2 gap-2 p-2 my-2  md:min-w-[60vw] lg:min-w-[30vw]  xs:min-w-[70vw]  min-w-[90vw] max-w-[40vw] rounded-xl'>
                         {/* DIV1 */}
                         <div className='flex flex-col items-start justify-start gap-2'>
-                                <div className='bg-[#D9D9D9] rounded-xl '>
-                                    No of Devices: <span className='text-xl font-bold'>09</span>
+                                <div className='bg-[#D9D9D9] rounded-xl sm:px-8 px-2 py-3 text-md flex items-center justify-center '>
+                                    No of Devices: <span className=' font-bold text-[#818181] px-2 text-2xl'>09</span>
                                 </div>
-                                <div className='grid grid-cols-2 gap-2'>
-                                        <div className='bg-[#6CCD87] p-5 rounded-xl'>
-                                            <h1>Pass</h1>
-                                            <h1>06</h1>
+                                <div className='grid  grid-cols-2 gap-2'>
+                                        <div className='bg-[#6CCD87] p-5 px-[29px] rounded-xl flex items-center justify-center flex-col '>
+                                            <h1 className='text-xl '>Pass</h1>
+                                            <h1 className='font-bold text-2xl text-white '>06</h1>
                                         </div>
-                                        <div className='bg-[#FAB09A] p-5 rounded-xl'>
-                                            <h1>Fail</h1>
-                                            <h1>06</h1>
+                                        <div className='bg-[#FAB09A] p-5  px-[29px] rounded-xl flex items-center justify-center flex-col '>
+                                            <h1 className='text-xl '>Fail</h1>
+                                            <h1 className='font-bold text-2xl text-white '>06</h1>
                                         </div>
                                 </div>
                         </div>
                         {/* DIV2 */}
-                            <div className='bg-[#2C509A] rounde-xl'>    
+                            <button className='bg-[#2C509A] rounded-xl flex items-center justify-center p-2 text-xl text-white'>    
                                 OTA
-                            </div>
+                            </button>
                 </div>
                 </div>
             </div>
@@ -165,3 +271,31 @@ function Page() {
 }
 
 export default Page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
